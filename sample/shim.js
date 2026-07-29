b0f3df53f090dd0ebc66dc5c72e00a548130b1b0
Johny Ho <johny@thoughtbot.com>
2026-07-28 22:58:57 -0400

    Update shim.js

--- sample/shim.js
+++ sample/shim.js
@@ -1,28 +1,18 @@
-export { TextEncoder, TextDecoder } from 'text-encoding'
-export { URL, URLSearchParams } from 'whatwg-url'
-
-export function MessageChannel() {
-  this.port1 = {
-    postMessage: function (message) {
-      console.log('Message sent from port1:', message)
-    },
-  }
-
-  this.port2 = {
-    addEventListener: function (event, handler) {
-      console.log(`Event listener added for ${event} on port2`)
-      this._eventHandler = handler
-    },
-    removeEventListener: function (event) {
-      console.log(`Event listener removed for ${event} on port2`)
-      this._eventHandler = null
-    },
-    simulateMessage: function (data) {
-      if (this._eventHandler) {
-        this._eventHandler({ data })
-      }
-    },
-  }
+// MiniRacer globals shim
+// These globals are referenced at module init time by React's streaming
+// build (bundled alongside renderToString) but not actually used by
+// renderToString itself. They only need to exist, not be functional.
+globalThis.TextEncoder = globalThis.TextEncoder || function () {
+  this.encode = function (s) { return new Uint8Array(0) }
+  this.encodeInto = function (s, d) { return { read: 0, written: 0 } }
 }
-
-export const navigator = { language: 'en-us' }
+globalThis.TextDecoder = globalThis.TextDecoder || function () {
+  this.decode = function () { return '' }
+}
+globalThis.queueMicrotask = globalThis.queueMicrotask || function (cb) { Promise.resolve().then(cb) }
+globalThis.crypto = globalThis.crypto || {}
+globalThis.navigator = globalThis.navigator || { language: 'en-us' }
+globalThis.MessageChannel = globalThis.MessageChannel || function () {
+  this.port1 = { postMessage() {} }
+  this.port2 = { addEventListener() {}, removeEventListener() {} }
+}
